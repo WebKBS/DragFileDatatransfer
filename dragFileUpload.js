7cf7a37b2b37498ff6dbddccd3487ff235208748
강범수 <kbs21c1987@gamil.com>
2023-02-13 15:41:41 +0900

    드롭 파일 이미지 업로드 완성

--- dragFileUpload.js
+++ dragFileUpload.js
@@ -1,61 +1,179 @@
-// 파일 업로드
-const fileSelector = document.getElementById('upload');
-const uploadLabel = document.querySelector('.upload_label');
-const fileName = document.getElementById('file_name');
-const fileMsg = document.getElementById('file_msg');
-const checkUploadBtn = document.getElementById('check_upload_btn');
+const uploadInput = document.getElementById('upload');
+const uploadLabel = document.querySelector('label[for="upload"]');
 
-const fileExtention = ['text/csv'];
+/* 기본 업로드시 이벤트 change */
+uploadInput.addEventListener('change', changeHandler);
 
-const fileHandler = (fileList, target) => {
-  if (fileList[0].type === fileExtention[0]) {
-    fileName.textContent = fileList[0].name;
-    fileMsg.textContent = '파일 업로드 완료';
-    uploadLabel.classList.add('on');
-  } else {
-    errorMsg();
-  }
-};
+/* 드래그 이벤트 */
+uploadLabel.addEventListener('dragenter', dragEnterHandler);
+uploadLabel.addEventListener('dragleave', dragLeaveHandler);
+uploadLabel.addEventListener('dragover', dragOverHandler);
+uploadLabel.addEventListener('drop', dropHandler);
 
-const errorMsg = () => {
+/**
+ * dragenter 이벤트 함수
+ * 드래그 된 요소가 드롭 대상에 들어갈 때
+ */
+function dragEnterHandler(event) {
+  event.preventDefault();
+  event.stopPropagation();
+}
+
+/**
+ * dragleave 이벤트 함수
+ * 드래그된 요소가 드롭 타겟 떠날때 ex) mouseleave
+ */
+function dragLeaveHandler(event) {
+  event.preventDefault();
+  event.stopPropagation();
   uploadLabel.classList.remove('on');
-  uploadLabel.classList.add('error');
-  fileName.textContent = '올바르지 않은 파일입니다.';
-  fileMsg.textContent = '다시 업로드 해주세요.';
-  fileSelector.value = '';
-};
+}
 
-fileSelector.addEventListener('change', (event) => {
-  const fileList = event.target.files;
-  fileHandler(fileList, event.target);
-});
-
-uploadLabel.addEventListener('dragover', (event) => {
-  event.stopPropagation();
+/**
+ * dragover 이벤트 함수
+ * 드래그 된 요소가 드롭 대상 위에있을 때 ex) mouseenter
+ */
+function dragOverHandler(event) {
   event.preventDefault();
-  uploadLabel.classList.add('move');
-  event.dataTransfer.dropEffect = 'copy';
-});
-
-uploadLabel.addEventListener('dragleave', (event) => {
   event.stopPropagation();
-  event.preventDefault();
-  uploadLabel.classList.remove('move');
-});
+  uploadLabel.classList.add('on');
+}
 
-uploadLabel.addEventListener('drop', (event) => {
+/**
+ * drop 이벤트 함수
+ * 드래그 된 요소가 드롭 되었을때.
+ */
+function dropHandler(event) {
+  event.preventDefault();
   event.stopPropagation();
-  event.preventDefault();
-  uploadLabel.classList.remove('move');
-  const fileList = event.dataTransfer?.files;
-  const data = new DataTransfer();
-  data.items.add(fileList[0]);
 
-  if (data.files[0].type === fileExtention[0]) {
-    fileSelector.files = data.files;
-    fileHandler(fileList, event.target);
-  } else {
-    data.items.clear();
-    errorMsg();
-  }
-});
+  let files = event.dataTransfer.files; // 드래그 파일 리스트
+
+  handleFiles(files);
+  viewImageHandler([...files]);
+  uploadLabel.classList.remove('on');
+}
+
+/**
+ * Input 체인지 이벤트 핸들러
+ */
+function changeHandler(event) {
+  let files = event.target.files; // input 파일 리스트
+  handleFiles(files);
+  viewImageHandler([...files]);
+}
+
+/**
+ * 파일 핸들러
+ * @param files - 들어올 file list
+ */
+function handleFiles(files) {
+  [...files].forEach(uploadFile);
+}
+
+/**
+ * form data 파일 업로드
+ *  */
+function uploadFile(file) {
+  let formData = new FormData();
+  formData.append('file', file);
+
+  let url = ''; // 전송 url
+
+  // fetch(url, {
+  //   method: 'POST',
+  //   body: formData,
+  // });
+}
+
+/**
+ * 이미지 닫기 클릭시 이미지 item 삭제
+ * @param target - 삭제 시킬 대상 부모 li
+ */
+function closeImageHandler(target) {
+  const removeTargetElement = target.querySelector('button');
+  removeTargetElement.addEventListener('click', () => {
+    removeTargetElement.parentElement.remove();
+  });
+}
+
+/**
+ *  FileReader 브라우저 뷰에 이미지 보여주기
+ * @param {*} fileList - input 및 드래그에 들어가는 파일 요소
+ */
+function viewImageHandler(fileList) {
+  fileList.forEach((file) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', (event) => {
+      const elements = createElement(event, file);
+
+      document.querySelector('#imageList ul').append(elements);
+      expandImage(elements);
+      closeImageHandler(elements);
+    });
+    reader.readAsDataURL(file);
+  });
+}
+window.addEventListener('load', () => {});
+
+/**
+ * 파일 생성시 엘리먼트 추가
+ * @param event - event 대상
+ */
+function createElement(event) {
+  const li = document.createElement('li');
+  const button = document.createElement('button');
+  const img = document.createElement('img');
+  img.setAttribute('src', event.target.result);
+  button.className = 'image_close_btn';
+  li.append(button);
+  li.append(img);
+  return li;
+}
+
+// addFunc(boolean, "block", src)
+// function addFunc(isDisable, style = 'none', img) {
+//   const input = document.getElementById('upload');
+//   const button = document.querySelector('.file_delete');
+//   const image = document.querySelector('.label_img_wrap');
+//   const imgElement = document.createElement('img');
+//   const textWrap = document.querySelector('.text_wrap');
+
+//   image.setAttribute('data-toggle', 'modal');
+//   image.setAttribute('data-target', '.bd-example-modal-lg');
+//   input.disabled = isDisable;
+//   button.style.display = style;
+//   textWrap.style.display = 'none';
+//   image.append(imgElement);
+//   image.querySelector('img').src = img;
+// }
+
+function expandImage(ele) {
+  const target = ele.querySelector('img');
+  target.addEventListener('click', () => {
+    const eleChildImage = target.src;
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = eleChildImage;
+    div.append(img);
+    div.classList.add('view_modal');
+    div.style.position = 'fixed';
+    div.style.top = '0px';
+    div.style.left = '0px';
+    div.style.backgroundColor = 'rgba(0, 0, 0, .7)';
+    div.style.width = '100%';
+    div.style.height = '100%';
+    div.style.zIndex = '2000';
+    img.style.maxWidth = '80%';
+    img.style.position = 'absolute';
+    img.style.top = '50%';
+    img.style.left = '50%';
+    img.style.transform = 'translate(-50%, -50%)';
+    document.body.append(div);
+    document.querySelector('.view_modal').addEventListener('click', (ev) => {
+      if (ev.target === document.querySelector('.view_modal')) {
+        document.querySelector('.view_modal').remove();
+      }
+    });
+  });
+}
